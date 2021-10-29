92d629935cdf400163e90fdbe0184cab9c0e44f6
May-Hemade <may.hemade1993@gmail.com>
2021-10-29 17:54:46 +0300

    Extra- homework-D5
    what  I was able to finish so far

---
+++ Day05/M1-D5-Extra.js
@@ -0,0 +1,131 @@
+// Additional assignments for Day 5
+
+/* EXTRA 1
+ Write a function called "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5 or not.
+ The function should return the sum of just the numbers bigger than 5.
+*/
+
+const giveMeRandom = function(n) {
+    let result = []
+    for (let i = 0; i< n; i++) {
+        result.push(getRandomInt(0, 10))
+    }
+    return result
+}
+
+const getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const checkArray = function(randomArray){
+    let sumNumb = 0
+for ( let numb of randomArray) {
+    if (numb> 5){
+        console.log(`${numb} bigger than 5`)
+        sumNumb+=numb
+    } else {
+        console.log(`${numb} smaller or equal to  5`)
+    }
+} 
+   return sumNumb
+}
+console.log (checkArray(giveMeRandom(7)))
+
+/* EXTRA 2
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "shoppingCartTotal" which calculates the total due to the shop.
+*/
+let item1 = {
+    name: "item1",
+    id:"123",
+    quantity:3,
+    price: 5
+}
+
+let item2 = {
+    name: "item2",
+    id:"12345",
+    quantity:7,
+    price: 4
+
+}
+
+
+
+const shoppingCartTotal = function(shoppingCart){ 
+    let sum=0
+    for( let item of shoppingCart) {
+       sum+= item.price*item.quantity
+    }
+    return sum
+}
+console.log (shoppingCartTotal([item1,item2]))
+
+/* EXTRA 3
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 4
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in it.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 5
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Create a function called "loopUntil" which receives an integer x between 0 and 9 as a parameter.
+ The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 7
+ Write a function called "average" which receives an array and returns the average numerical value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 8
+ Write a function called "longest" to find the longest string from a given array of strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 9
+ Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+ The function should return true if the emailContent string does not contain the words SPAM or SCAM.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 10
+ Write a function that receives a date as a parameter and calculates the number of days passed since the given date.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 11
+ Write a function called "matrixGenerator" which receives two integers, x and y, as parameters.
+ The result should be a matrix of x times y with, as value, the index of the position.
+ Ex.: x = 3, y = 2
+ ["00","01","02"
+ "10","11","12"]
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
+*/
